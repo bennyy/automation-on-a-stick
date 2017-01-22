4c85dac2cef86fc11189a32ca1e9d863eee75fd5
Benjamin <andersson.benjamin@gmail.com>
2017-01-22 13:39:19 +0100

    Change port to 14895

--- web/app.py
+++ web/app.py
@@ -21,5 +21,5 @@ def lights():
     return (request.form['light'], None)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=14895)
 
