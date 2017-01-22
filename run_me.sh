c97d6ff6d524d0a96d7ed4cce7350d4c71505195
Benjamin <andersson.benjamin@gmail.com>
2017-01-22 15:34:42 +0100

    Only kill Flask in run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export HOME_AUTO_DIR=/home/pi/dev/homeauto
+
 echo "Killing old processes.."
-ps aux | grep python | awk '{print $2}' | xargs kill
+ps aux | grep flask | awk '{print $2}' | xargs kill
 sleep 2
 echo "Starting server.."
-source /home/pi/dev/homeauto/env/bin/activate
-cd /home/pi/dev/homeauto/web/
-nohup /home/pi/dev/homeauto/web/start.sh &
+source ${HOME_AUTO_DIR}/env/bin/activate
+cd ${HOME_AUTO_DIR}/web/
+nohup ${HOME_AUTO_DIR}/web/start.sh &
 echo "Done"
