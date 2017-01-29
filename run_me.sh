a600e034adb8159ff5e762f62901c3d45f015b9c
Benjamin <andersson.benjamin@gmail.com>
2017-01-29 11:53:52 +0100

    Adapt run_me.sh for virtualenvwrapper

--- run_me.sh
+++ run_me.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-export HOME_AUTO_DIR=/home/pi/dev/homeauto
+export HOME_AUTO_DIR=/home/pi/apps/automation-on-a-stick
+
+# virtualenvwrapper thing
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
 
 echo "Killing old processes.."
 ps aux | grep flask | awk '{print $2}' | xargs kill
 sleep 2
 echo "Starting server.."
-source ${HOME_AUTO_DIR}/env/bin/activate
+workon homeauto
 cd ${HOME_AUTO_DIR}/web/
 nohup ${HOME_AUTO_DIR}/web/start.sh &
 echo "Done"
